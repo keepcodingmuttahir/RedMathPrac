05889b1501fa9979f269b5b373f4d37a0cb223f9
RM-WKS-W94\hassan.raza <hassan.raza@gmail.com>
2023-08-21 15:29:14 +0500

    This is my first message

---
+++ src/test/java/com/redmath/session1/ApplicationTest/ApplicationTest.java
@@ -0,0 +1,29 @@
+package com.redmath.session1.ApplicationTest;
+
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;            //mevan beans
+
+    @Test
+    public void testHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/index.html"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content().string(
+                        Matchers.startsWith("<!DOCTYPE html>")));
+    }
+}
